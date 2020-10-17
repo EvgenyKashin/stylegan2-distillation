29e319f2d29c3a7771320a2bdd1fca28716d7a2c
Mo <58116522+Metropass@users.noreply.github.com>
2020-10-17 14:13:50 -0400

    Fixes Depreciation Warning for Python 3.9+
    `fractions.gcd()` has been depreciated as of Python 3.9, we must used `math.gcd()` this will future proof the code for Python versions 3.9+, and added backwards compatibility for Python 3.8 and lower.

--- pix2pixHD/train.py
+++ pix2pixHD/train.py
@@ -5,8 +5,11 @@ import torch
 from torch.autograd import Variable
 from collections import OrderedDict
 from subprocess import call
-import fractions
-def lcm(a,b): return abs(a * b)/fractions.gcd(a,b) if a and b else 0
+try:
+    from fractions import gcd
+except ImportError:
+    from math import gcd
+def lcm(a,b): return abs(a * b)/gcd(a,b) if a and b else 0
 
 from options.train_options import TrainOptions
 from data.data_loader import CreateDataLoader
